1e00d1bddb41154715c6b0880c5998f2ec7d1d4a
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2025-11-27 14:51:17 +0530

    Update Salary.py

--- Salary.py
+++ Salary.py
@@ -5,8 +5,9 @@ if len(sys.argv)==2:
 else:
   script_name=sys.argv[0]
   salary=25000
-  bonus=salary*0.10
-  total_salary=salary+bonus
-  print("Salary:",salary)
-  print("Bonus:",bonus)
-  print("Total Salary:",total_salary)
+
+bonus=salary*0.10
+total_salary=salary+bonus
+print("Salary:",salary)
+print("Bonus:",bonus)
+print("Total Salary:",total_salary)
